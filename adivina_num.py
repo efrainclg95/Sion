445f9e06bc1fae53b81bfc7ada8675abc85a44c2
efrainclg95 <59065630+efrainclg95@users.noreply.github.com>
2020-03-12 11:43:53 -0500

    Delete adivina_num.py

--- adivina_num.py
+++
@@ -1,7 +0,0 @@
-number_pc = 34
-ingresa_num = int(input("ingrese un numero: "))
-
-if number_pc == ingresa_num:
-    print("Ganaste el numero es :{}" .format(number_pc))
-else:
-    print("Sigue intentando")
