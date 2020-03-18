2f154f0f6f8bcfd55bd350fc152542c40ef2a28c
efrainclg95 <efrainclg95@gmail.com>
2020-03-17 22:12:23 -0500

    Ejercicios de Listas

---
+++ Ejercicios_Web/Listas y Tuplas/ejercicio1.py
@@ -0,0 +1,13 @@
+# Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua)
+# en una lista y la muestre por pantalla.
+
+print ("Ingrese 5 Asignaturas")
+v_lista = []
+
+for i in range(1,6):
+    v_asignatura = input("Ingrese asignatura " + str(i) + ": ")
+    v_lista.append(v_asignatura)
+
+print(v_lista)
+# print(v_lista[0])
+
