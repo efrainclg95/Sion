fb8f62f2e6ef54e3bae977cbae32c31f824e7d3e
efrainclg95 <efrainclg95@gmail.com>
2020-03-20 21:46:18 -0500

    Ejercicios de Listas

---
+++ Ejercicios_Web/Listas y Tuplas/ejercicio5.py
@@ -0,0 +1,38 @@
+# Escribir un programa que almacene en una lista los números del 1 al 10 y los muestre por pantalla en orden inverso separados por comas.
+
+# Sol1
+
+# lista = []
+
+# for i in range(11,0,-1):
+    # lista.append(i)
+    # print(str(i),end= ", ")
+
+# print("")
+
+# print("Lista: " + str(lista))
+
+# Sol2
+
+lista = [1,2,3,4,5,6,7,8,9,10]
+lista.reverse()
+
+for i in lista:
+    print(str(i), end= ", ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
