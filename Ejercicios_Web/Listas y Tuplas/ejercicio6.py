c150d5cf1fa95a1f2504f7b1551f4cfc97831ccf
efrainclg95 <efrainclg95@gmail.com>
2020-03-23 16:31:55 -0500

    Ejercicios de Listas

---
+++ Ejercicios_Web/Listas y Tuplas/ejercicio6.py
@@ -0,0 +1,22 @@
+# Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista,
+# pregunte al usuario la nota que ha sacado en cada asignatura y elimine de la lista las asignaturas aprobadas.
+# Al final el programa debe mostrar por pantalla las asignaturas que el usuario tiene que repetir.
+
+asignaturas = ["Matemáticas", "Física", "Química", "Historia", "Lengua"]
+nota_asignatura = []
+
+# print(len(asignaturas))
+
+for i in range(len(asignaturas)):
+    v_nota = int(input("Ingrese nota de " + asignaturas[i] + ": "))
+    nota_asignatura.append(v_nota)
+# print(nota_asignatura)
+
+for j in range(len(nota_asignatura)):
+    if nota_asignatura[j] <= 10:
+        nota_asignatura.append(j)
+        print("Notas desaprobadas: " + str(nota_asignatura[j]))
+    # print("nota: " + str(nota_asignatura[j]))
+
+
+
