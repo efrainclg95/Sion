a5d228a2984430b356669e39a43e025ed9d0c60c
efrainclg95 <efrainclg95@gmail.com>
2020-03-30 10:33:12 -0500

    Ejercicios de Listas

--- Ejercicios_Web/Listas y Tuplas/ejercicio8.py
+++ Ejercicios_Web/Listas y Tuplas/ejercicio8.py
@@ -19,3 +19,22 @@ if lista_x == lista_y:
     print("Es una palabra palíndromo")
 else:
     print("No es un palíndromo")
+
+# Python code to demonstrate working of
+# reversed()
+
+# For string
+# seqString = "geeks"
+# print(list(reversed(seqString)))
+
+# For tuple
+# seqTuple = ('g', 'e', 'e', 'k', 's')
+# print(list(reversed(seqTuple)))
+
+# For range
+# seqRange = range(1, 5)
+# print(list(reversed(seqRange)))
+
+# For list
+# seqList = [1, 2, 4, 3, 5]
+# print(list(reversed(seqList)))
