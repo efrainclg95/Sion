642d2830d6fedee493cc831b4de9d9c448ba8c7d
efrainclg95 <efrainclg95@gmail.com>
2020-04-03 15:54:48 -0500

    Ejercicios de Listas

--- Ejercicios_Web/Listas y Tuplas/ejercicio11.py
+++ Ejercicios_Web/Listas y Tuplas/ejercicio11.py
@@ -4,17 +4,29 @@ v1 = (1,2,3)
 v2 = (-1,0,2)
 lv1 = []
 lv2 = []
-
-# Sol1
+p = int(0)
 
 for i in v1:
     lv1.append(i)
-print(lv1)
 
 for j in v2:
     lv2.append(j)
-print(lv2)
 
-p = (lv1[0] * lv2[0]) + (lv1[1] * lv2[1]) + (lv1[2] * lv2[2])
+for k in range(len(lv1)):
+    p = (lv1[k]*lv2[k]) + p
+print("El producto escalar es: " + str(p))
 
-print(p)
+# Sol1
+
+# for i in v1:
+    # lv1.append(i)
+# print(lv1)
+
+# for j in v2:
+    # lv2.append(j)
+# print(lv2)
+
+# p = (lv1[0] * lv2[0]) + (lv1[1] * lv2[1]) + (lv1[2] * lv2[2])
+# print(p)
+
+# Sol2
