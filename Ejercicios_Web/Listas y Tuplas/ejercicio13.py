25ed23229376a76f2fb7b4e9421b2afbcdb17dd8
efrainclg95 <efrainclg95@gmail.com>
2020-04-07 14:37:42 -0500

    Ejercicios de Listas

--- Ejercicios_Web/Listas y Tuplas/ejercicio13.py
+++ Ejercicios_Web/Listas y Tuplas/ejercicio13.py
@@ -2,7 +2,23 @@
 # los guarde en una lista y muestre por pantalla su media y desviación típica.
 
 v_num = input("Introduce una muestra de números separados por comas: ")
-v_num = v_num.split()
 
-for i in v_num:
-    print("Valor de i: " + i)
+# v_num = str("4,3,5,8,5,2,3,5")
+v_num = v_num.split(",")
+v_ancho = len(v_num)
+v_media = float(0)
+
+for i in range(v_ancho):
+    v_media = float(v_media) + int(v_num[i])/v_ancho
+
+print("La media es: " + str(v_media))
+
+
+
+
+
+
+
+
+
+
