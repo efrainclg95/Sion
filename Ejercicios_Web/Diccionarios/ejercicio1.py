106b56cea69b9b5c1d59701b8a457ae4f77d83c6
efrainclg95 <efrainclg95@gmail.com>
2020-04-13 11:50:57 -0500

    Ejercicios de Diccionarios

--- Ejercicios_Web/Diccionarios/ejercicio1.py
+++ Ejercicios_Web/Diccionarios/ejercicio1.py
@@ -1,17 +1,24 @@
 # Escribir un programa que guarde en una variable el diccionario {'Euro':'€', 'Dollar':'$', 'Yen':'¥'},
 # pregunte al usuario por una divisa y muestre su símbolo o un mensaje de aviso si la divisa no está en el diccionario.
 
-diccionario = {'Euro' : ['€'],
-               'Dollar': ['$'],
-               'Yen' : ['¥']
-               }
+# sol1
 
+# diccionario = {'Euro' : ['€'], 'Dollar': ['$'], 'Yen' : ['¥']}
+# v_divisa = str(input("Ingrese Divisa: "))
+
+# if v_divisa == 'Euro' or v_divisa == 'Dollar' or v_divisa == 'Yen' :
+    # print("Su símbolo es: " + str(diccionario[v_divisa]))
+
+# else:
+    # print("La Divisa ingresada no existe en el diccionario")
+
+# Sol2
+
+diccionario = {'Euro' : ['€'], 'Dollar': ['$'], 'Yen' : ['¥']}
 v_divisa = str(input("Ingrese Divisa: "))
 
-if v_divisa == 'Euro' or v_divisa == 'Dollar' or v_divisa == 'Yen' :
-    print("Su símbolo es: " + str(diccionario[v_divisa]))
+print(diccionario.get(v_divisa.title(), "la divisa no esta"))
 
-else:
-    print("La Divisa ingresada no existe en el diccionario")
+
 
 
