1afce495c153d290fc9a927a1626aadba1a8b52a
efrainclg95 <efrainclg95@gmail.com>
2020-04-13 11:03:02 -0500

    Ejercicios de Listas

--- Ejercicios_Web/Diccionarios/ejercicio1.py
+++ Ejercicios_Web/Diccionarios/ejercicio1.py
@@ -6,4 +6,11 @@ diccionario = {'Euro' : ['€'],
                'Yen' : ['¥']
                }
 
-print(diccionario['Euro'])
+v_divisa = str(input("Ingrese Divisa: "))
+
+if v_divisa == 'Euro' or v_divisa == 'Dollar' or v_divisa == 'Yen' :
+    print("Su símbolo es: " + str(diccionario[v_divisa]))
+else:
+    print("La Divisa ingresada no existe en el diccionario")
+
+
