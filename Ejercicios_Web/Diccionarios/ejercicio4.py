85ba5e4437baa611f9fc8be5f494de7f8a9ba9e2
efrainclg95 <efrainclg95@gmail.com>
2020-04-14 15:46:00 -0500

    Ejercicios de Diccionarios

--- Ejercicios_Web/Diccionarios/ejercicio4.py
+++ Ejercicios_Web/Diccionarios/ejercicio4.py
@@ -5,3 +5,4 @@ v_fecha = str(input("Introduce una fecha en formato dd/mm/aaaa: "))
 lista = v_fecha.split('/')
 dic = {'01' : "Enero", '02' : "Febrero", '03' : "Marzo", '04' : "Abril", '05' : "Mayo", '06' : "Junio", '07' : "Julio", '08' : "Agosto", '09' : "Septiembre", '10' : "Octubre", '11' : "Noviembre", '12' : "Diciembre"}
 
+print(lista[0] + " de " + dic.get(lista[1]) + " de " + lista[2])
