9b13338bee18f40a6faf409debf4ffa7d1532211
efrainclg95 <efrainclg95@gmail.com>
2020-04-14 12:34:31 -0500

    Ejercicios de Diccionarios

--- Ejercicios_Web/Diccionarios/ejercicio3.py
+++ Ejercicios_Web/Diccionarios/ejercicio3.py
@@ -7,11 +7,27 @@
 # Pera          0.85
 # Naranja       0.70
 
+# Sol1
+
+# v_fruta = str(input("Escriba que fruta desea comprar: "))
+# v_kilos = int(input("Cuantos kilos desea llevar: "))
+# dic = {'Plátano' : 1.35, 'Manzana' : 0.80, 'Pera' : 0.85, 'Naranja' : 0.70}
+
+# print("Va a llevar: " + v_fruta + " " + str(v_kilos) + " Kilos, un total de: " + str(dic.get(v_fruta)*v_kilos))
+
+# Sol2
+
 v_fruta = str(input("Escriba que fruta desea comprar: "))
 v_kilos = int(input("Cuantos kilos desea llevar: "))
 dic = {'Plátano' : 1.35, 'Manzana' : 0.80, 'Pera' : 0.85, 'Naranja' : 0.70}
 
-print("Va a llevar: " + v_fruta + " " + str(v_kilos) + " Kilos, un total de: " + str(dic.get(v_fruta)*v_kilos))
+if v_fruta in dic:
+    print("Va a llevar: " + v_fruta + " " + str(v_kilos) + " Kilos, un total de: " + str(dic.get(v_fruta) * v_kilos))
+else:
+    print("Fruta no en lista")
+
+
+
 
 
 
