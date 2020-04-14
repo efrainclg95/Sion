a1fd52d169fb1dd2f22348ca3aac8b2b5100112b
efrainclg95 <efrainclg95@gmail.com>
2020-04-14 15:41:28 -0500

    Ejercicios de Diccionarios

---
+++ Ejercicios_Web/Diccionarios/ejercicio4.py
@@ -0,0 +1,7 @@
+# Escribir un programa que pregunte una fecha en formato dd/mm/aaaa y muestre por pantalla la misma fecha en
+# formato dd de <mes> de aaaa donde <mes> es el nombre del mes.
+
+v_fecha = str(input("Introduce una fecha en formato dd/mm/aaaa: "))
+lista = v_fecha.split('/')
+dic = {'01' : "Enero", '02' : "Febrero", '03' : "Marzo", '04' : "Abril", '05' : "Mayo", '06' : "Junio", '07' : "Julio", '08' : "Agosto", '09' : "Septiembre", '10' : "Octubre", '11' : "Noviembre", '12' : "Diciembre"}
+
