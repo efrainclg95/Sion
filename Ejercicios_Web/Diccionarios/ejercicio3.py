2729bf66d333c1e5b9e2ed126301cf5ceff5e18f
efrainclg95 <efrainclg95@gmail.com>
2020-04-14 12:03:14 -0500

    Ejercicios de Diccionarios

---
+++ Ejercicios_Web/Diccionarios/ejercicio3.py
@@ -0,0 +1,21 @@
+# Escribir un programa que guarde en un diccionario los precios de las frutas de la tabla, pregunte al usuario por una fruta,
+# un número de kilos y muestre por pantalla el precio de ese número de kilos de fruta.
+# Si la fruta no está en el diccionario debe mostrar un mensaje informando de ello.
+# Fruta         Precio
+# Plátano       1.35
+# Manzana       0.80
+# Pera          0.85
+# Naranja       0.70
+
+v_fruta = str(input("Escriba que fruta desea comprar: "))
+v_kilos = int(input("Cuantos kilos desea llevar: "))
+dic = {'Plátano' : 1.35, 'Manzana' : 0.80, 'Pera' : 0.85, 'Naranja' : 0.70}
+
+# v_fruta = str("Manzana")
+# v_kilos = int(2)
+
+print("Va a llevar: " + v_fruta + " " + str(v_kilos) + " Kilos, un total de: " + str(dic.get(v_fruta)*v_kilos),"La fruta ingresada no esta en lista")
+
+
+
+
