5d7917e7d99228fba32ebdf4366d0847e6ca729a
efrainclg95 <efrainclg95@gmail.com>
2020-04-15 13:31:57 -0500

    Ejercicios de Diccionarios

---
+++ Ejercicios_Web/Diccionarios/ejercicio5.py
@@ -0,0 +1,10 @@
+# Escribir un programa que almacene el diccionario con los créditos de las asignaturas de un curso
+# {'Matemáticas': 6, 'Física': 4, 'Química': 5} y después muestre por pantalla los créditos de cada asignatura
+# en el formato <asignatura> tiene <créditos> créditos, donde <asignatura> es cada una de las asignaturas del curso, y
+# <créditos> son sus créditos. Al final debe mostrar también el número total de créditos del curso.
+
+dic_asignatura = {'Matemáticas' : 6 , 'Física' : 4, 'Química' : 5}
+
+for i in dic_asignatura:
+    print(i + " tiene " + str(dic_asignatura.get(i)) + " créditos")
+
