8f9232be84d25826d03ba9de512cfd058c895771
efrainclg95 <efrainclg95@gmail.com>
2020-04-16 15:01:24 -0500

    Ejercicios de Diccionarios

---
+++ Ejercicios_Web/Diccionarios/ejercicio6.py
@@ -0,0 +1,16 @@
+# Escribir un programa que cree un diccionario vacío y lo vaya llenando con información sobre una persona
+# (por ejemplo nombre, edad, sexo, teléfono, correo electrónico, etc.)
+# que se le pida al usuario. Cada vez que se añada un nuevo dato debe imprimirse el contenido del diccionario.
+
+dic = {}
+
+v_dato = str(input("Que dato quieres introducir: ").upper() + ": ")
+v_d1 = str(input(v_dato) + " ").upper()
+
+dic[v_dato] = v_d1
+
+print(dic)
+
+
+
+
