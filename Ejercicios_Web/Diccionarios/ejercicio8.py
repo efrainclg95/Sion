784b86113f13c134cb7a99cd11fe202338f732be
efrainclg95 <efrainclg95@gmail.com>
2020-04-21 16:34:38 -0500

    Ejercicios de Diccionarios

--- Ejercicios_Web/Diccionarios/ejercicio8.py
+++ Ejercicios_Web/Diccionarios/ejercicio8.py
@@ -11,9 +11,10 @@ lista2 = []
 lista3 = []
 
 lista4 = []
+lista5 = []
 
 # palabra = str(input("Introduce la lista de palabras y traducciones en formato <<palabra:traducción>> separadas por comas:"))
-palabra = str("perro:dog,blanco:white,esta:its,jugando:playing")
+palabra = str("perro:dog,blanco:white,esta:its,jugando:playing,casa:house")
 
 lista1 = palabra.split(',')
 
@@ -23,14 +24,28 @@ for i in range(len(lista1)):
 
     dic[lista2[0]] = lista2[1]
 
-# print(dic)
+print(dic)
+#print("lista2: " + str(lista2))
+#print("lista3: " + str(lista3))
 
 # frase = str(input("Ingrese frase: "))
-frase = str("Mi Pibe el perro blanco esta jugando")
+frase = str("Mi Pibe el perro blanco esta jugando en la casa")
+lista4 = frase.split()
+# print(lista4)
 
-for j in frase:
-    lista4 = frase.split()
-print(lista4)
+for j in dic:
+    # print("valor de j: " + j)
+    for k in lista4:
+        # print("valor de k: " + k)
+        if j == k:
+            lista5 = dic.get(j)
+            print(lista5)
+
+
+
+
+
+
 
 
 
