c3de4e9504ccd6ef4fa3780d2e10129fb502f91c
efrainclg95 <efrainclg95@gmail.com>
2020-04-24 15:55:17 -0500

    Ejercicios de Diccionarios

---
+++ Ejercicios_Web/Diccionarios/ejercicio9.py
@@ -0,0 +1,26 @@
+# Escribir un programa que gestione las facturas pendientes de cobro de una empresa.
+# Las facturas se almacenarán en un diccionario donde la clave de cada factura será el número de factura y el valor el coste de la factura.
+# El programa debe preguntar al usuario si quiere añadir una nueva factura, pagar una existente o terminar.
+# Si desea añadir una nueva factura se preguntará por el número de factura y su coste y se añadirá al diccionario.
+# Si se desea pagar una factura se preguntará por el número de factura y se eliminará del diccionario.
+# Después de cada operación el programa debe mostrar por pantalla la cantidad cobrada hasta el momento y la cantidad pendiente de cobro.
+
+v_recaudado = int(0)
+v_pendiente = int(0)
+v_pregunta = str("A")
+dic = {}
+
+while v_pregunta == "A":
+    v_pregunta = input("¿Quieres añadir una nueva factura (A), pagarla (P) o terminar (T)? ")
+    if v_pregunta == "A":
+        v_numfac = int(input("Introduce el número de la factura: "))
+        v_costfac = int(input("Introduce el coste de la factura: "))
+        dic[v_numfac] = v_costfac
+        print("Diccionario temporal: " + str(dic))
+
+
+
+
+print("Recaudado: " + str(v_recaudado))
+print("Pendiente de cobro: " + str(v_pendiente))
+
