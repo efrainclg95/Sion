acb6a22762b1ac1eaad7a22e193d2f3b75d8ab94
efrainclg95 <efrainclg95@gmail.com>
2020-05-04 17:54:15 -0500

    Ejercicios de Repaso

---
+++ Ejercicios_Web/1 Repaso/ejercicio6.py
@@ -0,0 +1,9 @@
+# Escribir un programa que almacene en una lista los siguientes precios, 50, 75, 46, 22, 80, 65, 8,
+# y muestre por pantalla el menor y el mayor de los precios.
+
+list = [50,75,46,22,80,65,8]
+
+v_max = max(list)
+v_min = min(list)
+
+print(v_max,v_min)
