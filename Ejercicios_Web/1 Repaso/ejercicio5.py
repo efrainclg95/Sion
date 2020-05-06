36066d45a803d5d2dcd47b1a48dee994a04ab42e
efrainclg95 <efrainclg95@gmail.com>
2020-05-05 22:50:01 -0500

    Ejercicios de Repaso

---
+++ Ejercicios_Web/1 Repaso/ejercicio5.py
@@ -0,0 +1,18 @@
+# Escribir un programa que almacene los vectores (1,2,3) y (-1,0,2) en dos listas y muestre por pantalla su producto escalar.
+
+t1 = (1,2,3)
+t2 = (-1,0,2)
+l1 = []
+l2 = []
+
+for i in t1:
+    l1.append(i)
+for j in t2:
+    l2.append(j)
+
+
+
+# for n,k in enumerate(l1):
+    # print(n,k)
+
+# print(l1,l2)
