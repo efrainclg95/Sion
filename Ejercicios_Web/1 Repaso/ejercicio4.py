d2ce08c548993bfe0b7538ac63bcb8e3094f6089
efrainclg95 <efrainclg95@gmail.com>
2020-05-08 15:33:16 -0500

    Ejercicios de repaso

--- Ejercicios_Web/1 Repaso/ejercicio4.py
+++ Ejercicios_Web/1 Repaso/ejercicio4.py
@@ -7,20 +7,18 @@
 a = ((1,2,3),(4,5,6))
 b = ((-1,0),(0,1),(1,1))
 
-la = []
-lb = []
+result = [[0,0],
+          [0,0]]
 
 for i in range(len(a)):
-    la.append(list(a[i]))
-
-print("lista la: " + str(la))
-
-for j in range(len(b)):
-    lb.append(list(b[j]))
-
-print("lisya lb: " + str(lb))
-
-print(la[0[0]])
+    for j in range(len(b[0])):
+        for k in range(len(b)):
+            result[i][j] += a[i][k] * b[k][j]
+for i in range(len(result)):
+    result[i] = tuple(result[i])
+result = tuple(result)
+for i in range(len(result)):
+    print(result[i])
 
 
 
