44f9a9eb4ec791a759dc7d24fe39e00c496ff499
efrainclg95 <efrainclg95@gmail.com>
2020-05-13 12:34:43 -0500

    Ejercicios de Funciones

---
+++ Ejercicios_Web/Funciones/test.py
@@ -0,0 +1,46 @@
+
+v_num = 12
+v_mul = 5
+
+for v_num in range(0,v_num+1):
+    p = v_num * v_mul
+    print(str(v_mul) + 'x' + str(v_num) + '=' + str(p))
+
+
+
+
+
+"""def f_num(n):
+    for i in range(2,n + 1,2):
+        print(i)
+       
+
+f_num(10)"""
+
+"""v_edad = int(input("Ingrese edad: "))
+
+def mayor_edad(v_edad):
+    if v_edad >= 18:
+        print("Eres mayor de edad")
+    else:
+        print("Eres un bebé")
+        return
+
+mayor_edad(v_edad)"""
+
+
+"""Suma
+y = 9
+x = 7
+
+def f_suma (x,y):
+
+    s = x + y
+    print(s)
+    return
+
+f_suma(x,y)"""
+
+
+
+
