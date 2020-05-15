c13ff78b8b97c235ce08f98f4b38134e983f1ed2
efrainclg95 <efrainclg95@gmail.com>
2020-05-15 17:07:56 -0500

    Ejercicios de Funciones

--- Ejercicios_Web/Funciones/ejercicio5.py
+++ Ejercicios_Web/Funciones/ejercicio5.py
@@ -1,14 +1,27 @@
 # Escribir una función que calcule el área de un círculo y
 # otra que calcule el volumen de un cilindro usando la primera función.
 
+# Sol 1
+
+def area_circulo(r):
+    pi = 3.14159
+    return pi * r**2
+
+def volumen_cilindro(r,h):
+    return area_circulo(r)*h
+
+print(area_circulo(80))
+print(volumen_cilindro(80,500))
+
+
+""""# Sol 2
+
 radio = float(input("Ingrese radio de circulo: "))
 
 def area_circulo(radio):
     pi = 3.14159
-    # radio = float(input("Ingrese radio de circulo: "))
     global v_acirculo
     v_acirculo = pi * radio**2
-    # return 'Area de circulo: ' + str(v_acirculo)
     return 'Area de circulo: ' + str(v_acirculo)
 
 print(area_circulo(radio))
@@ -20,4 +33,4 @@ def volumen_cilindro(h):
     v_cilindro = v_acirculo * h
     return 'Volumen de cilindro: ' + str(v_cilindro)
 
-print(volumen_cilindro(h))
+print(volumen_cilindro(h))"""
