6e0745ca6d4e722bda353aacbba1b8558a64417d
efrainclg95 <efrainclg95@gmail.com>
2020-05-18 15:27:39 -0500

    Ejercicios de Funciones

---
+++ Ejercicios_Web/Funciones/ejercicio6.py
@@ -0,0 +1,9 @@
+# Escribir una función que reciba una muestra de números en una lista y devuelva su media.
+
+lista = []
+
+def media(lista):
+    return sum(lista)/len(lista)
+
+print(media([1,2,3]))
+
