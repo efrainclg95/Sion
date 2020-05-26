fd701215a92868f5480f2baafadefb2d424a743e
efrainclg95 <efrainclg95@gmail.com>
2020-05-26 11:36:02 -0500

    Ejercicios de Funciones

--- Ejercicios_Web/Funciones/ejercicio10.py
+++ Ejercicios_Web/Funciones/ejercicio10.py
@@ -1,18 +1,16 @@
 # Escribir una función que convierta un número decimal en binario y
 # otra que convierta un número binario en decimal.
 
-def to_binary(n):
-    """Función que convierte un número decimal en binario.
-    Parámetros:
-        - n: Es un número entero.
-    Devuelve:
-        El número binario correspondiente a n.
-    """
-    binary = []
-    while n > 0:
-        binary.append(str(n % 2))
-        n //= 2
-    binary.reverse()
-    return ''.join(binary)
+n = 35
 
-print(to_binary(4))
+listai = []
+listar = []
+
+while n > 0:
+
+    listai.append(n%2)
+    n = n//2
+
+for i in reversed(listai):
+    listar.append(i)
+print(listar)
