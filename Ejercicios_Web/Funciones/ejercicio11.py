128d913ab7c1a443101b6500d5f3cb37bec71013
efrainclg95 <efrainclg95@gmail.com>
2020-05-30 19:55:31 -0500

    Ejercicios de Funciones

--- Ejercicios_Web/Funciones/ejercicio11.py
+++ Ejercicios_Web/Funciones/ejercicio11.py
@@ -1,7 +1,8 @@
 # Escribir un programa que reciba una cadena de caracteres y devuelva un diccionario con cada palabra que contiene y su frecuencia.
 # Escribir otra función que reciba el diccionario generado con la función anterior y devuelva una tupla con la palabra más repetida y su frecuencia.
 
-texto = 'Felipe es es un niño hermoso y es bebito bello niño'
+texto = 'Felipe es un niño hermoso y es bebito bello niño es'
+
 dic = {}
 lista = texto.split()
 
@@ -12,3 +13,7 @@ for j,k in enumerate(lista):
     dic[k] = contador
 
 print(dic)
+
+
+for keys,values in dic.items():
+    print(keys,values)
