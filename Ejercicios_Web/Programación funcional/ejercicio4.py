50abb41f8cab433a040a2247cb7f520a33286e3e
efrainclg95 <efrainclg95@gmail.com>
2020-06-05 11:41:31 -0500

    Ejercicios de Programación Funcional

--- Ejercicios_Web/Programación funcional/ejercicio4.py
+++ Ejercicios_Web/Programación funcional/ejercicio4.py
@@ -1,6 +1,8 @@
 '''Escribir una función que reciba otra función booleana y una lista,
 y devuelva otra lista con los elementos de la lista que devuelvan True al aplicarles la función booleana.'''
 
+''''# Sol 1
+
 def f_bool(funcion,lista):
 
     lp = []
@@ -17,6 +19,19 @@ def f_bool(funcion,lista):
 def f_par(n):
     return n % 2 == 0
 
-# print(f_par(5))
+print(f_bool(f_par,[1,2,3,4,5,6]))'''
+
+# Sol 2
+
+def f_bool(funcion,lista):
+
+    lp = []
+
+    for i in lista:
+        lp.append(funcion(i))
+    return lp
+
+def f_par(n):
+    return n % 2 == 0
 
 print(f_bool(f_par,[1,2,3,4,5,6]))
