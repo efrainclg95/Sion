c9d02035d9fa27767348120f34cf0c29ea65b53f
efrainclg95 <efrainclg95@gmail.com>
2020-06-08 15:21:05 -0500

    Ejercicios de funciones

--- Ejercicios_Web/Programación funcional/ejercicio5.py
+++ Ejercicios_Web/Programación funcional/ejercicio5.py
@@ -1,9 +1,9 @@
 """Escribir una función que reciba una frase y devuelva un diccionario con las palabras que contiene y su longitud."""
 
 frase = 'Felipe bello bebe'
-lista_frase = []
 
 def f_out_dic(frase):
+    lista_frase = []
     for i in frase:
         lista_frase = frase.split()
     return lista_frase
