367c9d727900d7653e890708182af45c038185b4
efrainclg95 <efrainclg95@gmail.com>
2020-06-08 15:19:13 -0500

    Ejercicios de funciones

--- Ejercicios_Web/Programación funcional/ejercicio5.py
+++ Ejercicios_Web/Programación funcional/ejercicio5.py
@@ -8,5 +8,12 @@ def f_out_dic(frase):
         lista_frase = frase.split()
     return lista_frase
 
-print(f_out_dic(frase))
+def f_longitud_frase(lista):
+    dic = {}
+    for j,k in enumerate(f_out_dic(frase)):
+        dic[f_out_dic(frase)[j]] = len(k)
+    return dic
+
+#print(f_out_dic(frase))
+print(f_longitud_frase(f_out_dic(frase)))
 
