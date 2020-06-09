ca617b38f199c05781d7d94bc4c1cc0001334d61
efrainclg95 <efrainclg95@gmail.com>
2020-06-09 16:30:39 -0500

    Ejercicios de funciones

---
+++ Ejercicios_Web/Programación funcional/ejercicio6.py
@@ -0,0 +1,8 @@
+"""Escribir una función reciba una lista de notas y devuelva la lista de calificaciones correspondientes a esas notas."""
+
+def notas(n):
+    if n < 5:
+        return 'SS'
+
+print(notas(4))
+
