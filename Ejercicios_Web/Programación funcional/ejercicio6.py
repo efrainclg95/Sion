a3b1df5c327ea128a1a1d73aca18ecf4166c0fcb
efrainclg95 <efrainclg95@gmail.com>
2020-06-09 17:11:28 -0500

    Ejercicios de funciones

--- Ejercicios_Web/Programación funcional/ejercicio6.py
+++ Ejercicios_Web/Programación funcional/ejercicio6.py
@@ -1,5 +1,7 @@
 """Escribir una función reciba una lista de notas y devuelva la lista de calificaciones correspondientes a esas notas."""
 
+''''# Sol 1
+
 def califica(n):
 
     if n < 5:
@@ -21,5 +23,24 @@ def notas(lista):
         lista_califica.append(califica(i))
     return lista_califica
 
-print(notas([4,5,3.2,9,10]))
+print(notas([4,5,3.2,9,10]))'''
 
+# Sol 2
+
+def califica(n):
+
+    if n < 5:
+        return 'SS'
+    elif n < 7:
+        return 'AP'
+    elif n < 9:
+        return 'NT'
+    elif n < 10:
+        return 'SB'
+    else:
+        return 'MH'
+
+def notas(lista):
+    return list(map(califica,lista))
+
+print(notas([4,5,3.2,9,10]))
