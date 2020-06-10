3cc71ae1e783223bcd71d36a19e114541698b348
efrainclg95 <efrainclg95@gmail.com>
2020-06-10 17:17:54 -0500

    Ejercicios de programacion funcional

--- Ejercicios_Web/Programación funcional/ejercicio7.py
+++ Ejercicios_Web/Programación funcional/ejercicio7.py
@@ -1,6 +1,8 @@
 """Escribir una función reciba un diccionario con las asignaturas y las notas de un alumno y devuelva otro diccionario con las asignaturas en mayúsculas y
 las calificaciones correspondientes a las notas."""
 
+# Sol 1
+
 def califica(n):
 
     if n < 5:
@@ -14,4 +16,17 @@ def califica(n):
     else:
         return 'MH'
 
-print(califica(10))
+def dic_califica(dic):
+    out_dic = {}
+    for keys,values in dic.items():
+        out_dic[keys.upper()] = califica(values)
+        # print(keys,values)
+    return out_dic
+
+dic = {'matemáticas': 10, 'lenguaje' : 3.5}
+
+print(dic_califica(dic))
+
+
+
+
