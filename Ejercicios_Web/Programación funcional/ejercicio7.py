0b891e4977b6f7fef2ff350b3baffdc5c62483c3
efrainclg95 <efrainclg95@gmail.com>
2020-06-10 23:25:00 -0500

    Ejercicios de programacion funcional

--- Ejercicios_Web/Programación funcional/ejercicio7.py
+++ Ejercicios_Web/Programación funcional/ejercicio7.py
@@ -3,7 +3,7 @@ las calificaciones correspondientes a las notas."""
 
 # Sol 1
 
-def califica(n):
+'''def califica(n):
 
     if n < 5:
         return 'SS'
@@ -25,8 +25,32 @@ def dic_califica(dic):
 
 dic = {'matemáticas': 10, 'lenguaje' : 3.5}
 
-print(dic_califica(dic))
+print(dic_califica(dic))'''
 
 
+# Sol 2
 
+def califica(n):
 
+    if n < 5:
+        return 'SS'
+    elif n < 7:
+        return 'AP'
+    elif n < 9:
+        return 'NT'
+    elif n < 10:
+        return 'SB'
+    else:
+        return 'MH'
+
+def dic_califica(scores):
+
+    asuntos = map(str.upper, scores.keys())
+    grades = map(califica, scores.values())
+
+    print('asuntos: ',asuntos)
+    print('grades: ',grades)
+
+    return dict(zip(asuntos, grades))
+
+print(dic_califica({'Matemáticas':6.5, 'Física':5, 'Química':3.4, 'Economía':8.2, 'Historia':9.7, 'Programación':10}))
