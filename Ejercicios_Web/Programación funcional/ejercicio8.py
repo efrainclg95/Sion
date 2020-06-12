0fdaa0916881ab06064588b225d558efaf85697a
efrainclg95 <efrainclg95@gmail.com>
2020-06-11 23:19:59 -0500

    Ejercicios de programacion funcional

--- Ejercicios_Web/Programación funcional/ejercicio8.py
+++ Ejercicios_Web/Programación funcional/ejercicio8.py
@@ -2,6 +2,10 @@
 y devuelva otro diccionario con las asignaturas en mayúsculas
 y las calificaciones correspondientes a las notas aprobadas."""
 
+# Sol 1
+
+""""
+
 def califica(n):
 
     if n < 5:
@@ -23,5 +27,33 @@ def dic_califica(scores):
             dic_aprobados[keys.upper()] = califica(values)
     return dic_aprobados
 
-print('Calificaciones aprobadas: ',dic_califica({'Matemáticas':6.5, 'Física':5, 'Química':3.4, 'Economía':8.2, 'Historia':9.7, 'Programación':10}))
+print(dic_califica({'Matemáticas':6.5, 'Física':5, 'Química':3.4, 'Economía':8.2, 'Historia':9.7, 'Programación':10}))"""
+
+# Sol 2
+
+def califica(n):
+
+    if n < 5:
+        return 'SS'
+    elif n < 7:
+        return 'AP'
+    elif n < 9:
+        return 'NT'
+    elif n < 10:
+        return 'SB'
+    else:
+        return 'MH'
+
+def nota_aprobada(nota):
+    return (nota[1] >= 5) # duda con esta función
+
+def dic_califica(dic_notas):
+
+    proceso = dict(filter(nota_aprobada, dic_notas.items()))
+    asignatura = map(str.upper,proceso.keys())
+    puntaje = map(califica,proceso.values())
+
+    return dict(zip(asignatura,puntaje))
+
+print(dic_califica({'Matemáticas': 6.5, 'Física': 5, 'Química': 3.4, 'Economía': 8.2, 'Historia': 9.7, 'Programación': 10}))
 
