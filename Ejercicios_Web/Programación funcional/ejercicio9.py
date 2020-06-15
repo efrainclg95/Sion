f53e16924c0201179b85e363b6846f30b6345575
efrainclg95 <efrainclg95@gmail.com>
2020-06-15 14:58:53 -0500

    Ejercicios de programacion funcional

--- Ejercicios_Web/Programación funcional/ejercicio9.py
+++ Ejercicios_Web/Programación funcional/ejercicio9.py
@@ -1,7 +1,9 @@
 """Escribir una función que calcule el módulo de un vector."""
 
 def modulo_vector(tupla):
+    modulo = 0
     for i in tupla:
-        print(i)
+        modulo = modulo + i**2
+    return modulo
 
 print(modulo_vector((3,4)))
