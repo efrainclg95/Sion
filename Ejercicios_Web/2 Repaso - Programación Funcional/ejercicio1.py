0222c2c852ca63a5c2201676ad47e69644e69f5a
efrainclg95 <efrainclg95@gmail.com>
2020-06-17 21:26:24 -0500

    Ejercicios de repaso programacion funcional con diccionarios

---
+++ Ejercicios_Web/2 Repaso - Programación Funcional/ejercicio1.py
@@ -0,0 +1,19 @@
+"""Escribir un programa que guarde en una variable el diccionario {'Euro':'€', 'Dollar':'$', 'Yen':'¥'},
+pregunte al usuario por una divisa y muestre su símbolo o un mensaje de aviso si la divisa no está en el diccionario."""
+
+
+v_divisa = str(input("Indique divisa:\nEuro\nDollar\nYen\nEscriba: "))
+
+def divisa(v_divisa):
+    dic = {'Euro':'€', 'Dollar':'$', 'Yen':'¥'}
+    return list(dic.get(v_divisa))
+
+print(divisa(v_divisa))
+
+
+
+
+
+
+
+
