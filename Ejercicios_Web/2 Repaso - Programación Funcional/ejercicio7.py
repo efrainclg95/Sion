567392756d7cc22a03de7c6fcb8e2c26b750ce71
efrainclg95 <efrainclg95@gmail.com>
2020-06-22 11:41:08 -0500

    Ejercicios de repaso programacion funcional con diccionarios

--- Ejercicios_Web/2 Repaso - Programación Funcional/ejercicio7.py
+++ Ejercicios_Web/2 Repaso - Programación Funcional/ejercicio7.py
@@ -7,16 +7,28 @@ articulo2     Precio
 articulo3     Precio
 Total         Costo"""
 
-v_articulo = str(input('Que articulo desea comprar: '))
-v_precio = float(input('Ingrese precio del artículo: '))
+print('LISTA DE COMPRAS')
+
+v_articulo = str
+v_precio = float
 
 def f_dic_compras(v_articulo,v_precio):
     v_continua = 'Si'
     dic = {}
     while v_continua == 'Si':
-
+        v_articulo = str(input('Que articulo desea comprar: '))
+        v_precio = float(input('Ingrese precio del artículo: '))
         dic[v_articulo] = v_precio
         v_continua = str(input('Desea continuar con las compras (Si/No): '))
     return dic
 
-print(f_dic_compras(v_articulo,v_precio))
+def f_costo(diccionario):
+    v_total = 0
+    for keys,values in diccionario.items():
+        v_total += values
+        cadena = 'Total de compras S/.: ',str(v_total)
+        cadena = ''.join(cadena)
+    return cadena
+
+print(f_costo(f_dic_compras(v_articulo,v_precio)))
+
