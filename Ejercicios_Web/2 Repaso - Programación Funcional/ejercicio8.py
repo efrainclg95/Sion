7f23f954bdd44a506f373049ec06bd07b68af18e
efrainclg95 <efrainclg95@gmail.com>
2020-06-25 01:47:15 -0500

    Ejercicios de repaso programacion funcional con diccionarios

--- Ejercicios_Web/2 Repaso - Programación Funcional/ejercicio8.py
+++ Ejercicios_Web/2 Repaso - Programación Funcional/ejercicio8.py
@@ -6,20 +6,31 @@ El programa debe crear un diccionario con las palabras y sus traducciones.
 Después pedirá una frase en español y utilizará el diccionario para traducirla palabra a palabra.
 Si una palabra no está en el diccionario debe dejarla sin traducir."""
 
-# v_palabra = str(input('Ingrese palabras <palabra>:<traducción>: '))
-v_palabra = 'el:the,carro:car,papá:father,es:is'
+v_traductor = 'el:the,carro:car,papá:father,es:is'
 
-def f_palabra(v_palabra):
+def f_palabra(v_traductor):
     dic = {}
     lista2 = []
-    lista1 = v_palabra.split(',')
+    lista1 = v_traductor.split(',')
     for i in lista1:
         lista2 = i.split(':')
         dic[lista2[0]] = lista2[1]
     return dic
 
+def f_traduccion(v_frase):
+    lista3 = v_frase.split()
+    for n,k in enumerate(lista3):
+        print(k)
 
-print(f_palabra(v_palabra))
+
+
+
+
+
+
+
+print(f_palabra(v_traductor))
+print(f_traduccion('el carro de papá es un volkswagen'))
 
 
 
