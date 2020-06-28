fa1277a4357250612f4cd071bc65b4a997f3d0ea
efrainclg95 <efrainclg95@gmail.com>
2020-06-28 18:07:03 -0500

    Ejercicios de repaso programacion funcional con diccionarios

--- Ejercicios_Web/2 Repaso - Programación Funcional/ejercicio9.py
+++ Ejercicios_Web/2 Repaso - Programación Funcional/ejercicio9.py
@@ -5,33 +5,46 @@ Si desea añadir una nueva factura se preguntará por el número de factura y su
 Si se desea pagar una factura se preguntará por el número de factura y se eliminará del diccionario.
 Después de cada operación el programa debe mostrar por pantalla la cantidad cobrada hasta el momento y la cantidad pendiente de cobro."""
 
-v_pregunta = str(input('F1 Añadir una nueva factura (A)\nPagarla (P)\nTerminar (T)\nIngrese: '))
+v_pregunta = str(input('Añadir una nueva factura (A)\nPagarla (P)\nTerminar (T)\nIngrese: '))
+dic = {}
 
-# while v_pregunta == 'A' or v_pregunta == 'P' or v_pregunta == 'T':
-    # print('True')
+def funcion0(v_pregunta):
+    v_pregunta = str(input('Añadir una nueva factura (A)\nPagarla (P)\nTerminar (T)\nIngrese: '))
+    return v_pregunta
 
 def funcion1(v_pregunta):
     while v_pregunta == 'A' or v_pregunta == 'P' or v_pregunta == 'T':
-        return v_pregunta
+        if v_pregunta == 'A':
+            v_num_factura = int(input('Introduce el número de la factura: '))
+            v_coste_factura = float(input('Introduce el coste de la factura: '))
+            dic[v_num_factura] = v_coste_factura
+            return (dic)
+        elif v_pregunta == 'P':
+
+
+
+
+
+
 
 
 print(funcion1(v_pregunta))
 
-'''v_pregunta = ''
 
-def funcion1(v_pregunta):
-
-    v_pregunta = str(input('F1 Añadir una nueva factura (A)\nPagarla (P)\nTerminar (T)\nIngrese: '))
-    return v_pregunta
-
-def funcion2(b_bucle):
-
-    if funcion1(v_pregunta) == 'A':
-        print('True A')
-    elif funcion1(v_pregunta) == 'P':
-        print('True P')
-    elif funcion1(v_pregunta) == 'T':
-        print('True T')
-
-# print(funcion1(v_pregunta))
-print(funcion2('b_bucle'))'''
+'''¿Quieres añadir una nueva factura (A), pagarla (P) o terminar (T)? A
+Introduce el número de la factura: 01
+Introduce el coste de la factura: 100
+Diccionario temporal: {1: 100}
+Recaudado: 0
+Pendiente de cobro: 100
+¿Quieres añadir una nueva factura (A), pagarla (P) o terminar (T)? A
+Introduce el número de la factura: 02
+Introduce el coste de la factura: 200
+Diccionario temporal: {1: 100, 2: 200}
+Recaudado: 0
+Pendiente de cobro: 300
+¿Quieres añadir una nueva factura (A), pagarla (P) o terminar (T)? P
+Introduce el número de la factura a pagar: 02
+Diccionario temporal borrado: {1: 100}
+Recaudado: 200
+Pendiente de cobro: 100'''
