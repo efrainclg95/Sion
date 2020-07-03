af00ba690cfaf799806b33f4de326d62c62ff29c
efrainclg95 <efrainclg95@gmail.com>
2020-07-03 00:19:06 -0500

    Ejercicios de repaso programacion funcional con diccionarios

--- Ejercicios_Web/2 Repaso - Programación Funcional/ejercicio9.py
+++ Ejercicios_Web/2 Repaso - Programación Funcional/ejercicio9.py
@@ -5,41 +5,26 @@ Si desea añadir una nueva factura se preguntará por el número de factura y su
 Si se desea pagar una factura se preguntará por el número de factura y se eliminará del diccionario.
 Después de cada operación el programa debe mostrar por pantalla la cantidad cobrada hasta el momento y la cantidad pendiente de cobro."""
 
-respuesta = str
 dic = {}
 
 def funcion1(respuesta):
+    v_recaudo = 0
     while respuesta == 'A' or respuesta == 'P':
         respuesta = str(input('¿Quieres añadir una nueva factura (A), pagarla (P) o terminar (T)?: '))
         if respuesta == 'A':
-            print('es A')
+            v_n_factura = int(input('Ingrese # factura: '))
+            v_c_factura = float(input('Ingrese costo de factura: '))
+            dic[v_n_factura] = v_c_factura
+            print(dic)
+
         elif respuesta == 'P':
-            print('es P')
+            v_nf_pagar = int(input('Ingrese # factura a pagar: '))
+            v_recaudo += dic.get(v_nf_pagar)
+            print('Recaudado: ',v_recaudo)
+            del dic[v_nf_pagar]
+            print(dic)
+
         elif respuesta == 'T':
-            print('BYE')
+            return 'BYE'
 
 print(funcion1('A'))
-
-
-
-
-
-
-
-'''¿Quieres añadir una nueva factura (A), pagarla (P) o terminar (T)? A
-Introduce el número de la factura: 01
-Introduce el coste de la factura: 100
-Diccionario temporal: {1: 100}
-Recaudado: 0
-Pendiente de cobro: 100
-¿Quieres añadir una nueva factura (A), pagarla (P) o terminar (T)? A
-Introduce el número de la factura: 02
-Introduce el coste de la factura: 200
-Diccionario temporal: {1: 100, 2: 200}
-Recaudado: 0
-Pendiente de cobro: 300
-¿Quieres añadir una nueva factura (A), pagarla (P) o terminar (T)? P
-Introduce el número de la factura a pagar: 02
-Diccionario temporal borrado: {1: 100}
-Recaudado: 200
-Pendiente de cobro: 100'''
