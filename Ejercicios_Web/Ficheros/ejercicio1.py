1cb50826bbe089b56971b2f837021a93b243f87a
efrainclg95 <efrainclg95@gmail.com>
2020-07-07 17:34:28 -0500

    Ejercicios de ficheros

--- Ejercicios_Web/Ficheros/ejercicio1.py
+++ Ejercicios_Web/Ficheros/ejercicio1.py
@@ -2,26 +2,26 @@
 y guarde en un fichero con el nombre tabla-n.txt la tabla de multiplicar de ese número,
 donde n es el número introducido."""
 
-'''def input_num(n):
+# Sol 1
+
+def input_num(n):
 
     file_name = 'tabla-' + str(n) + '.txt'
     f = open(file_name, 'w')
 
     for i in range(1,13):
         tabla = i * n
-        # print(n,' x ',i, '=',tabla)
-        f.write(n,' x ',i, '=',tabla)
+        # print(n,' x ',i, ' = ',tabla)
+        f.write(str(n) + ' x ' + str(i) + ' = ' + str(tabla) + '\n')
     f.close()
 
-    # return
+print(input_num(int(input('Ingrese Número del 1 al 10: '))))
 
+# Sol 2
 
-print(input_num(int(input('Ingrese Número del 1 al 10: '))))'''
-
-
-n = int(input('Introduce un número entero entre 1 y 10: '))
+"""n = int(input('Introduce un número entero entre 1 y 10: '))
 file_name = 'tabla-' + str(n) + '.txt'
 f = open(file_name, 'w')
 for i in range(1, 11):
     f.write(str(n) + ' x ' + str(i) + ' = ' + str(n * i) + '\n')
-f.close()
+f.close()"""
