24707229773fa2397df9265f909fefe9d3754889
efrainclg95 <efrainclg95@gmail.com>
2020-07-07 16:56:23 -0500

    Ejercicios de ficheros

---
+++ Ejercicios_Web/Ficheros/ejercicio1.py
@@ -0,0 +1,25 @@
+"""Escribir una función que pida un número entero entre 1 y 10
+y guarde en un fichero con el nombre tabla-n.txt la tabla de multiplicar de ese número,
+donde n es el número introducido."""
+
+def input_num(n):
+
+    file_name = 'tabla-' + str(n) + '.txt'
+    f = open(file_name, 'w')
+
+    for i in range(1,13):
+        tabla = i * n
+        print(n,' x ',i, '=',tabla)
+
+    # return
+
+
+print(input_num(int(input('Ingrese Número del 1 al 10: '))))
+
+
+"""n = int(input('Introduce un número entero entre 1 y 10: '))
+file_name = 'tabla-' + str(n) + '.txt'
+f = open(file_name, 'w')
+for i in range(1, 11):
+    f.write(str(n) + ' x ' + str(i) + ' = ' + str(n * i) + '\n')
+f.close()"""
