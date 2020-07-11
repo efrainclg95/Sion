a749806bc3ddcec29928fbdc24bf634cfa9577ba
efrainclg95 <efrainclg95@gmail.com>
2020-07-10 22:47:32 -0500

    Ejercicios sobre ficheros

--- Ejercicios_Web/Ficheros/testp0.py
+++ Ejercicios_Web/Ficheros/testp0.py
@@ -1,4 +1,4 @@
-f = open('virus_1','w')
+'''f = open('virus_1','w')
 f.write('Es un virus 1\n')
 
 f = open('virus_1','a')
@@ -7,4 +7,16 @@ f.write('Felipe y sus padres aman a Dios')
 f = open('virus_1','r')
 print(f.read())
 
-f.close()
+f.close()'''
+
+import os
+f = 'virus_1_1.txt'
+if os.path.isfile(f):
+    os.rename(f, 'virus_1_1.txt')
+else:
+    print('el fichero ',f,' no existe')
+
+if os.path.isfile(f):
+    os.remove(f)
+else:
+    print('el fichero ',f,' no existe')
