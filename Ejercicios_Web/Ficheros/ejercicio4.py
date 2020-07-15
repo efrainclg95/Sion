27b591bfa1c8620bde23bf56633cc26b77bbe9d8
efrainclg95 <efrainclg95@gmail.com>
2020-07-14 20:09:54 -0500

    Ejercicios sobre ficheros

--- Ejercicios_Web/Ficheros/ejercicio4.py
+++ Ejercicios_Web/Ficheros/ejercicio4.py
@@ -20,5 +20,24 @@ print(num_palabras(lectura_url('https://www.gutenberg.org/cache/epub/2000/pg2000
 
 # Sol2
 
+def words_file(url):
+    '''
+    Función que recibe recibe la url de un fichero de texto y devuelve el número de palabras que contiene.
+    Parámetros:
+        url: Es una cadena con la url del fichero de texto.
+    Devuelve:
+        El número de palabras que contiene el fichero de texto daro por la url.
+    '''
+    from urllib import request
+    from urllib.error import URLError
+    try:
+        file = request.urlopen(url)
+    except URLError:
+        return('¡La url ' + url + ' no existe!')
+    else:
+        content = file.read()
+        return len(content.split())
 
+print(words_file('https://www.gutenberg.org/cache/epub/2000/pg2000.txt'))
+print(words_file('https://no-existe.txt'))
 
