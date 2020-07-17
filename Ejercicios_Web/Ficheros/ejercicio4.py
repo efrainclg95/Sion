867971460a6fe70a283bce791444460898e88a5e
efrainclg95 <efrainclg95@gmail.com>
2020-07-17 10:01:30 -0500

    Ejercicios sobre ficheros

--- Ejercicios_Web/Ficheros/ejercicio4.py
+++ Ejercicios_Web/Ficheros/ejercicio4.py
@@ -1,9 +1,9 @@
 """Escribir un programa que acceda a un fichero de internet mediante su url
 y muestre por pantalla el número de palabras que contiene."""
 
-# Sol1
+'''# Sol1
 
-'''from urllib import request
+from urllib import request
 
 def lectura_url(www):
     f = request.urlopen(www)
@@ -21,23 +21,16 @@ print(num_palabras(lectura_url('https://www.gutenberg.org/cache/epub/2000/pg2000
 # Sol2
 
 def words_file(url):
-    '''
-    Función que recibe recibe la url de un fichero de texto y devuelve el número de palabras que contiene.
-    Parámetros:
-        url: Es una cadena con la url del fichero de texto.
-    Devuelve:
-        El número de palabras que contiene el fichero de texto daro por la url.
-    '''
     from urllib import request
     from urllib.error import URLError
+
     try:
         file = request.urlopen(url)
     except URLError:
-        return('¡La url ' + url + ' no existe!')
+        return ('La url' + url + 'no existe')
     else:
         content = file.read()
         return len(content.split())
 
-print(words_file('https://www.gutenberg.org/cache/epub/2000/pg2000.txt'))
+print(words_file('https://www.gutenberg.org/cache/epub/2000/mpg2000.txt'))
 print(words_file('https://no-existe.txt'))
-
