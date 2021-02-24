42d2a629d3e622149e48ef6b95dfba060640ff52
efrainclg95 <efrainclg95@gmail.com>
2021-02-23 23:48:55 -0500

    Scraping covid

--- Ejercicios_Web/Web Scraping/covid.py
+++ Ejercicios_Web/Web Scraping/covid.py
@@ -7,8 +7,21 @@ page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 
 pais = soup.find_all('div', class_= 'pcAJd')
+cantidad = soup.find_all('td', class_= 'l3HOY')
 
-print(pais)
+paises = []
+infectados_hoy = []
+
+for p in pais:
+    paises.append(p.text)
+
+for i in cantidad:
+    infectados_hoy.append(i.text)
+
+# print(paises)
+print(infectados_hoy)
+
+
 
 
 
