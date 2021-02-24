8460e889240e4815b4fd34945009a9c535b8ea69
efrainclg95 <efrainclg95@gmail.com>
2021-02-24 16:59:30 -0500

    Scraping covid

--- Ejercicios_Web/Web Scraping/covid.py
+++ Ejercicios_Web/Web Scraping/covid.py
@@ -2,10 +2,6 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
-paises = []
-casos_hoy = []
-total_casos = []
-
 url = 'https://news.google.com/covid19/map?hl=es-419&gl=PE&ceid=PE%3Aes-419'
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
@@ -13,24 +9,45 @@ soup = BeautifulSoup(page.content, 'html.parser')
 pais = soup.find_all('div', class_= 'pcAJd')
 cantidad = soup.find_all('td', class_= 'l3HOY')
 
+paises = []
+total_casos = []
 
 for p in pais:
     paises.append(p.text)
 
-contador = 0
 
-while contador <= 1000:
-    contador = contador + 5
-    # print(contador)
-
-    for j, k in enumerate(cantidad):
-
+for j,k in enumerate(cantidad):
+    contador = -5
+    while contador < 300 :
+        contador = contador + 5
+        # print(j)
         if j == contador:
+            # print(j,k.text)
             total_casos.append(k.text)
 
-print(total_casos)
+# Solución en base a diccionarios
 
-        # print(j,k)
+'''dic_covid = dict(zip(paises,total_casos))
+
+v_in_pais = input(str('Ingrese país a consultar: '))
+v_out_resultado = str('La cantidad actual de casos COVID19 en '+ v_in_pais + ' es de: ' + dic_covid.get(v_in_pais))
+
+print(v_out_resultado)'''
+
+# Solución usando Pandas
+
+df = pd.DataFrame({'Pais':paises,'Total de Casos':total_casos})
+print(df)
+
+
+
+'''df = pd.DataFrame({'Nombre':equipos,'Puntos':puntos}, index = list(range(1,21)))
+
+print(df)'''
+
+
+
+
 
 
 
