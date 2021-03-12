c36a75005e51721dcb41799c8043ea25441e61cc
efrainclg95 <efrainclg95@gmail.com>
2021-03-12 17:01:59 -0500

    Ejercicios Pandas

--- Ejercicios_Web/Pandas/ejercicio1.py
+++ Ejercicios_Web/Pandas/ejercicio1.py
@@ -7,5 +7,18 @@ import pandas as pd
 v_inicial = int(input('Introduce el año incial: '))
 v_final = int(input('Introduce el año final: '))
 
+dic_ventas = {}
+
 for i in range(v_inicial,v_final + 1):
-    print(i)
+
+    v_ventas_i = int(input('Introduce las ventas del año ' + str(i) + ': '))
+
+    dic_ventas[i] = v_ventas_i
+
+print(dic_ventas)
+
+
+
+
+
+
