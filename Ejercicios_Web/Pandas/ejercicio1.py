6c9d956858ab45755a9049c704ef1c5ebd8f0cc8
efrainclg95 <efrainclg95@gmail.com>
2021-03-13 16:04:41 -0500

    Ejercicios Pandas

--- Ejercicios_Web/Pandas/ejercicio1.py
+++ Ejercicios_Web/Pandas/ejercicio1.py
@@ -4,7 +4,9 @@ antes y después de aplicarles un descuento del 10%.'''
 
 import pandas as pd
 
-v_inicial = int(input('Introduce el año incial: '))
+# Estructurado
+
+'''v_inicial = int(input('Introduce el año incial: '))
 v_final = int(input('Introduce el año final: '))
 
 dic_ventas = {}
@@ -17,7 +19,33 @@ for i in range(v_inicial,v_final + 1):
     s = pd.Series(dic_ventas)
 
 print('Ventas\n',s)
-print('Ventas con descuento del 10%\n', s * 0.9)
+print('Ventas con descuento del 10%\n', s * 0.9)'''
+
+# Funciones
+
+dic_ventas = {}
+
+def datos(inicial,final):
+
+    for i in range(inicial,final + 1):
+        v_ventas_i = int(input('Introduce las ventas del año ' + str(i) + ': '))
+
+        dic_ventas[i] = float(v_ventas_i)
+        s = pd.Series(dic_ventas)
+
+    s1 = print('Ventas\n',s)
+    s2 = print('Ventas con descuento\n', s * 0.9)
+
+    return s1,s2
+
+print(datos(2020,2021))
+
+
+
+
+
+
+
 
 
 
