f7e8b745e02df01eb07fbcb19be698d6d4852bf4
efrainclg95 <efrainclg95@gmail.com>
2021-03-15 15:38:04 -0500

    Ejercicios Pandas

---
+++ Ejercicios_Web/Pandas/ejercicio2.py
@@ -0,0 +1,9 @@
+'''Escribir una función que reciba una diccionario con las notas de los alumnos en curso en un examen y
+devuelva una serie con la nota mínima, la máxima, media y la desviación típica.'''
+
+import pandas as pd
+
+def serie_notas(diccionario):
+    return diccionario
+
+print(serie_notas({'Juan':9, 'María':6.5, 'Pedro':4, 'Carmen': 8.5, 'Luis': 5}))
