2e3cb4125d8adc534dee74859a91925fea0a0033
efrainclg95 <efrainclg95@gmail.com>
2021-03-19 12:18:22 -0500

    Ejercicios Pandas

---
+++ Ejercicios_Web/Pandas/ejercicio5.py
@@ -0,0 +1,12 @@
+'''Escribir una función que reciba un DataFrame con el formato del ejercicio anterior,
+una lista de meses, y devuelva el balance (ventas - gastos) total en los meses indicados.'''
+
+import pandas as pd
+
+def df_datos(dataframe):
+    df = pd.DataFrame(dataframe)
+
+    return df
+
+print(df_datos({'Mes':['Enero','Febrero','Marzo','Abril'],'Ventas':[30500,35600,28300,33900],'Gastos':[22000,23400,18100,20700]}))
+
