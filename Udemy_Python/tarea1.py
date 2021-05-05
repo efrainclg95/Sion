c9d62d37ce592b14652ad3f8d0588107940dfcbb
efrainclg95 <efrainclg95@gmail.com>
2021-05-05 13:23:29 -0500

    Tareas Udemy_Python

---
+++ Udemy_Python/tarea1.py
@@ -0,0 +1,22 @@
+# Números con Python
+
+# Ejercicio 1 Calcula la división entera de 45 entre 6
+
+divEntera = 45 // 6
+print(divEntera)
+
+# Ejercicio 2 Calcula el resto de la división entera de 45 entre 6
+
+divEnteraResto = 45 % 6
+print(divEnteraResto)
+
+# Ejercicio 3 Realiza la siguiente operación en Python, donde ÷ indica la división entera: 10 + 20 ÷ 7 − 2
+
+operacion3 = 10 + 20//7 - 2
+print(operacion3)
+
+# Ejercicio 4 Realiza la siguiente operación en Python
+
+operacion4 = (9 - ((25+5-2)/(7*4)))/(pow(2,3))
+print(operacion4)
+
