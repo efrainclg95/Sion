89f073dfed9c248d897be3f51202d3cd887eaa2f
efrainclg95 <efrainclg95@gmail.com>
2021-05-23 23:01:16 -0500

    Tareas Udemy Python Strings

--- Udemy_Python/Tarea4/ejercicio2.py
+++ Udemy_Python/Tarea4/ejercicio2.py
@@ -1,3 +1,3 @@
-v_texto = 'Érase un hombre a una nariz pegado,\nÉrase una nariz superlativa,\nÉrase una alquitara medio viva,\nÉrase un peje espada mal barbado;'
+v_txt = 'Érase un hombre a una nariz pegado,\nÉrase una nariz superlativa,\nÉrase una alquitara medio viva,\nÉrase un peje espada mal barbado;'
 
-print(v_texto)
+print(v_txt)
