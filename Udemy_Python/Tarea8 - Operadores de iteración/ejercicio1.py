d19f7628508dd1b0776977199a6fae29f0cff35c
efrainclg95 <efrainclg95@gmail.com>
2021-06-16 21:50:57 -0500

    Ejercicios operadores de iteración

---
+++ Udemy_Python/Tarea8 - Operadores de iteración/ejercicio1.py
@@ -0,0 +1,18 @@
+# Haz que el usuario introduzca números enteros por teclado. Mientras el usuario no introduzca el 0, muestra
+# si el número introducido es par o impar
+
+v_num_in = int(input('Ingrese número entero: '))
+
+while v_num_in != 0:
+
+    if v_num_in % 2 == 0:
+        print('Es par')
+        v_num_in = int(input('Ingrese número entero: '))
+
+    else:
+        print('Es impar')
+        v_num_in = int(input('Ingrese número entero: '))
+
+print('Ingresaste el valor prohibido, chau!!!')
+
+
