b72d99e1598e42467abca3c8f2dc3a169f83c2ad
efrainclg95 <efrainclg95@gmail.com>
2021-06-16 23:08:04 -0500

    Ejercicios Pandas

---
+++ Udemy_Python/Tarea8 - Operadores de iteración/ejercicio4.py
@@ -0,0 +1,19 @@
+# Haz que el usuario introduzca números enteros por teclado. Mientras el usuario no introduzca el 0, calcula
+# cuántos números positivos y cuántos negativos ha introducido y muéstraselo al final.
+
+v_num = int(input('Ingrese número: '))
+contador_p = 0
+contador_n = 0
+
+while v_num != 0:
+    if v_num > 0:
+        contador_p += 1
+    elif v_num < 0:
+        contador_n += 1
+    v_num = int(input('Ingrese número: '))
+
+else:
+    print('Fin de proceso')
+    print('Números positicos {} y números negativos {}'.format(contador_p,contador_n))
+
+
